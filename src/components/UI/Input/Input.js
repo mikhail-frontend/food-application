f6aacca41202f0e72e448229e8fefa6bbe001d6a
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2022-10-16 18:14:18 +0300

    add forward ref just in case

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,16 +1,32 @@
+import {forwardRef, useRef, useImperativeHandle} from "react";
 import styles from './Input.module.scss'
-const Input = ({className = '', id = '0', label = '', value = '', changeHandler = () => {}, ...htmlProps}) => {
+
+const Input = forwardRef(({
+                              className = '',
+                              id = '0',
+                              label = '', value = '', changeHandler = () => {
+    }, ...htmlProps  }) => {
+    const inputRef = useRef();
+    const onFocus = () => {
+        inputRef.current?.focus();
+    }
+    useImperativeHandle(inputRef, {
+        onFocus
+    });
+
     return (
         <div className={`${styles.input} ${className}`}>
             {label && <label htmlFor={id}>{label}</label>}
             <input type={htmlProps.type || 'text'}
                    id={id}
                    value={value}
+                   ref={inputRef}
                    onInput={(event) => changeHandler(event.target.value, id, 'input')}
                    onBlur={(event) => changeHandler(event.target.value, id, 'blur')}
                    {...htmlProps}
             />
         </div>
     )
-}
+});
+
 export default Input
