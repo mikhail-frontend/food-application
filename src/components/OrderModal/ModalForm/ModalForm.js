03e3dc23af20d25d6461f03fe108a3e9cc3be2a5
mikhail-frontend <mikhail.kharitonov2022.frontend@gmail.com>
2022-10-23 18:26:29 +0300

    start dev order form

---
+++ src/components/OrderModal/ModalForm/ModalForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+import useForm from "../../../hooks/use-form";
+import { signupForm } from "../../../helpers/formConfig";
+
+import "./styles.css";
+import "./SignupForm.css";
+
+const ModalForm = () => {
+    const { renderFormInputs, isFormValid } = useForm(signupForm);
+    return (
+        <form className="signupForm">
+            <h1>Sign Up</h1>
+            {renderFormInputs()}
+            <button type="submit" disabled={!isFormValid()}>
+                Submit
+            </button>
+        </form>
+    );
+}
+
+export default ModalForm;
